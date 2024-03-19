ea1a8b2d88dae5aa3a222c19cc9e79d12cea9a74
Irving Daniel Reyes Mayo <irvingdaniel.reyes@bbva.com>
2024-03-19 09:26:42 -0600

    Fin cap. 22 Operador lógico

---
+++ 02-operadores/04-logicos.js
@@ -0,0 +1,14 @@
+// AND, OR, NOT
+
+let mayor = false;
+let suscrito = true;
+// console.log(true && true);
+// console.log(false && true);
+console.log('Operador AND:', mayor && suscrito);
+
+//OR ||
+console.log('Operador OR:', mayor || suscrito);
+
+//NOT !
+console.log('Operador NOT:', !mayor);
+let soloCatalogoInfantil = !mayor;
