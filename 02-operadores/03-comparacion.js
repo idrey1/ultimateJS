4c0bc11e1601379988cb4d1decf773b7d31805a2
Irving Daniel Reyes Mayo <irvingdaniel.reyes@bbva.com>
2024-03-19 09:05:45 -0600

    Se agregan comentarios de cap 21 Operadores de comparación

--- 02-operadores/03-comparacion.js
+++ 02-operadores/03-comparacion.js
@@ -1,9 +1,12 @@
 let a = 10;
+
+//operadores relacionales
 console.log(a>5);
 console.log(a>=5);
 console.log(a<5);
 console.log(a<=5);
 
+//Operadores de igualdad
 console.log(a == 10);
 console.log(a != 10);
 console.log(a === '10');
