70179138aab87cc674f32b5a51dbd7a6374e7f11
Irving Daniel Reyes Mayo <irvingdaniel.reyes@bbva.com>
2024-03-19 02:14:36 -0600

    Fin cap. 19 Operadores aritmeticos

---
+++ 02-operadores/01-aritmeticos.js
@@ -0,0 +1,21 @@
+let a = 5;
+let b = 7;
+
+// console.log(a + b, 'adición'); //adición
+// console.log(a - b, 'resta'); //resta
+// console.log(a * b, 'multiplicación'); //multiplicación
+// console.log(a / b, 'división'); //división
+// console.log(a % b, 'módulo'); //módulo
+// console.log(a ** b, 'potencia') //potencia
+
+//incrementar
+console.log(++a); //incrementa valor de a en 1 y
+//luego lo muestra en la consola: 1+5=6
+console.log(a++); //6+1=7 
+console.log(a);
+
+//decrementar
+console.log(--a); //decrementa valor de a en 1 y
+//luego lo muestra en la consola: 1-7=6
+console.log(a--); //7-1=6 
+console.log(a); //5
