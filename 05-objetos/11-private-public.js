3044840032747e1968f8497f183c74965e2d4cc7
Irving Daniel Reyes Mayo <irvingdaniel.reyes@bbva.com>
2024-04-06 20:24:32 -0600

    Fin cap. 59 Privado vs público

---
+++ 05-objetos/11-private-public.js
@@ -0,0 +1,14 @@
+function Usuario(){
+    let id = 1;
+    this.name = 'Nicolas';
+    let log = function(){
+        console.log('logging...');
+    }
+    this.guardar = function(){
+        log();
+        console.log('guardando...');
+    }
+}
+
+const usuario = new Usuario();
+usuario.guardar();
