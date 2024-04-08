5510a89bf3faa1e4092516831892b707418b9f41
Irving Daniel Reyes Mayo <irvingdaniel.reyes@bbva.com>
2024-04-08 01:58:55 -0600

    Fin cap. 75 Eliminar elementos

---
+++ 07-arrays/02-eliminar.js
@@ -0,0 +1,11 @@
+const letras = ['a', 'b', 'c', 'd'];
+
+//final
+//const final = letras.pop();
+//console.log(final);
+
+//comienzo
+//const comienzo = letras.shift();
+//console.log(comienzo, letras);
+letras.splice(1, 2);
+console.log(letras);
