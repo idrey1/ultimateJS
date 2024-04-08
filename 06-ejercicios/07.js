fae5491b9e0453561ecb40e1722e05ef8251a3ff
Irving Daniel Reyes Mayo <irvingdaniel.reyes@bbva.com>
2024-04-08 01:26:50 -0600

    Fin cap. 72 Agregar ID

---
+++ 06-ejercicios/07.js
@@ -0,0 +1,11 @@
+let objeto = {name: 'Nicolas'};
+
+function agregarId(obj){
+    if(typeof obj==='object'){
+        obj.id = Math.random();
+    }
+    return obj;
+}
+
+agregarId(objeto);
+console.log(objeto);
