f733b74255d58e9b1c9ae75071281c490c11cd54
Irving Daniel Reyes Mayo <irvingdaniel.reyes@bbva.com>
2024-04-07 20:13:58 -0600

    Fin cap. 64 Template strings

---
+++ 05-objetos/16-templates.js
@@ -0,0 +1,10 @@
+const nombre = 'Nicolas';
+const apellido = 'Schurman'
+function plantilla (nombre){ 
+    return `Hola ${nombre}!
+Bienvenidos a "Ultimate Javascript" :)
+
+Cariños Nico."`;
+}
+
+console.log(plantilla('Chanchito feliz'));
