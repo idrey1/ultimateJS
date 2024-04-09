3fd80aafd702ee8a68b7ee2a9faf9fcbbefa13c0
Irving Daniel Reyes Mayo <irvingdaniel.reyes@bbva.com>
2024-04-09 01:53:05 -0600

    Fin cap. 82 Join

---
+++ 07-arrays/09-join.js
@@ -0,0 +1,7 @@
+let arr1 = ['Nicolas','Chanchito','Felipe'];
+let mensaje = arr1.join(', ');
+//console.log(mensaje);
+
+let saludo = 'Hola mundo! desde NZ';
+let dividido =  saludo.split(' ');
+console.log(dividido.join('-'));
